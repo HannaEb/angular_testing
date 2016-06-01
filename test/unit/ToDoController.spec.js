ed093457ab3f6b797303a1942296e2206cc06fbf
Hanna <hannaeberhoefer@yahoo.com>
2016-06-01 12:22:36 +0100

    Karma setup

---
+++ test/unit/ToDoController.spec.js
@@ -0,0 +1,15 @@
+describe('ToDoController', function() {
+  beforeEach(module('toDoApp'));
+
+  it('initialises with a toDo', function() {
+    expect(ctrl.todo).toEqual("ToDo1");
+  });
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ToDoController');
+  }));
+
+
+});
