a2b297f9f15c69612e21d451749e9444293a5c5a
Hanna <hannaeberhoefer@yahoo.com>
2016-06-01 11:01:12 +0100

    Added first feature tests

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -4,3 +4,10 @@ describe("app", function() {
     expect(browser.getTitle()).toEqual("Todos App");
   });
 });
+
+describe("app", function() {
+  it("should say 'Hello world' on the page", function() {
+    browser.get('/');
+    expect($$("p").first().getText()).toEqual("Hello world");
+  });
+});
